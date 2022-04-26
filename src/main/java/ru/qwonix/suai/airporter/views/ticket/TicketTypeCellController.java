ebd541e7db475048ec08276a87f6f8b495066edc
roman <roman.qwonix@gmail.com>
2022-04-27 02:30:31 +0300

    add TicketType info to ticket-type-cell-layout.fxml

--- src/main/java/ru/qwonix/suai/airporter/views/ticket/TicketTypeCellController.java
+++ src/main/java/ru/qwonix/suai/airporter/views/ticket/TicketTypeCellController.java
@@ -1,29 +1,57 @@
 package ru.qwonix.suai.airporter.views.ticket;
 
-import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 import ru.qwonix.suai.airporter.model.entity.TicketType;
 
+import java.time.format.DateTimeFormatter;
+
 @Component
 @FxmlView("ticket-type-cell-layout.fxml")
 public class TicketTypeCellController {
 
-    @FXML
-    public Label flightLabel, conditionLabel, ticketsLabel;
-    @FXML
-    protected Label idLabel;
 
-    @FXML
-    protected Label priceLabel;
+    public Button selectTicketButton;
+    public Label priceLabel;
+    public Label ticketsCountLabel;
+    public Label airlineLabel;
+    public Label conditionLabel;
+    public Label departureTimeLabel;
+    public Label departureCityLabel;
+    public Label departureDateLabel;
+    public Label flightDurationLabel;
+    public Label arrivalTimeLabel;
+    public Label arrivalCityLabel;
+    public Label arrivalDateLabel;
 
     public void CellSetup(TicketType ticketType) {
-        idLabel.setText(String.valueOf(ticketType.getId()));
-        flightLabel.setText(ticketType.getFlight().getDepartureAirport().getName() + " -- " + ticketType.getFlight().getArrivalAirport().getName());
+        priceLabel.setText(ticketType.getPrice() + "руб");
+        ticketsCountLabel.setText(ticketType.getTickets().size() + " билетов");
+
+        selectTicketButton.setOnMouseClicked(event -> {
+            System.out.println("select " + ticketType.getId());
+        });
+
+        airlineLabel.setText(ticketType.getFlight().getAircraft().getAirline().getName());
         conditionLabel.setText(ticketType.getCondition().getName());
-        priceLabel.setText(String.valueOf(ticketType.getPrice()));
-        ticketsLabel.setText(String.valueOf(ticketType.getTickets().size()));
+
+        departureTimeLabel.setText(ticketType.getFlight().getScheduledDeparture()
+                .format(DateTimeFormatter.ofPattern("h:mm")));
+        departureCityLabel.setText(ticketType.getFlight().getDepartureAirport().getCity());
+        departureDateLabel.setText(ticketType.getFlight().getScheduledDeparture()
+                .format(DateTimeFormatter.ofPattern("d MMM uuu, E")));
+
+        flightDurationLabel.setText("todo");
+
+        arrivalTimeLabel.setText(ticketType.getFlight().getScheduledArrival()
+                .format(DateTimeFormatter.ofPattern("h:mm")));
+        arrivalCityLabel.setText(ticketType.getFlight().getArrivalAirport().getCity());
+        arrivalDateLabel.setText(ticketType.getFlight().getScheduledArrival()
+                .format(DateTimeFormatter.ofPattern("d MMM uuu, E")));
+
+
     }
 }
 
